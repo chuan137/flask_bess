6f8004f89be1b4f593d9ca62938eb597cc454a2c
Chuan Miao <chuan137@gmail.com>
2016-02-17 12:41:15 +0100

    enable multiple db in one configuration

--- modules/sites/bess/api.py
+++ modules/sites/bess/api.py
@@ -2,6 +2,7 @@ from flask import Response
 from flask_restful import Resource
 from ...adeireader.adeireader import ADEIReader
 import os, json
+import copy
 from operator import itemgetter
 
 configurations = ['bess32', 'bess250', 'pvcn1000']
@@ -12,23 +13,25 @@ def open_config(conf):
     with open(os.path.join(cdir, fname)) as f:
         return json.load(f)
 
-def init_adei(conf):
-    conf = open_config(conf)
-    host, server, db = conf.get('url'), conf.get('server'), conf.get('database')
-    return ADEIReader(host, server, db)
+def init_adei(s):
+    return ADEIReader(s.get('url'), s.get('server'), s.get('database'))
 
-def ordered_sensor_list(conf):
-    conf = open_config(conf)
-    sensors = conf.get('sensors')
-    for grp, sns in sensors.iteritems():
-        for _ in sns:
-            _['sensor'] = '{}.{}'.format(grp, _['sensor'])
-    sensors = [_ for grp,sns in sensors.iteritems() for _ in sns]
-    return sorted(sensors, key=itemgetter('order'))
+def init_group(s):
+    res = {}
+    for grpname, sensors in s.iteritems():
+        res[grpname] = [_.get('sensor') for _ in sensors]
+    return res
 
-def sensor_list(conf):
-    conf = open_config(conf)
-    return {k:[_.get('sensor') for _ in v] for k,v in conf.get('sensors').iteritems()}
+def ordered_sensor(conf):
+    sensor_list = []
+    conf = copy.deepcopy(conf)
+    for c in conf:
+        for grp,sns in c['sensors'].iteritems():
+            for s in sns:
+                s['sensor'] = grp+'.'+s['sensor']
+            sensor_list.extend(sns)
+    sensor_list = sorted(sensor_list, key=itemgetter('order'))
+    return [_['sensor']  for _ in sensor_list]
 
 def output_html(data, code, headers=None):
     if type(data) is list:
@@ -37,17 +40,18 @@ def output_html(data, code, headers=None):
     resp.status_code = code
     return resp
 
+adei = {}
+for c in configurations:
+    conf = open_config(c)
+    if not isinstance(conf, list): conf = [ conf ]
 
-adeireader_obj = {s : init_adei(s) for s in configurations}
-sensors = {s: sensor_list(s)  for s in configurations}
-sensors_ordered = {s: ordered_sensor_list(s) for s in configurations}
+    _ordered = ordered_sensor(conf)
+    adei[c] = dict(db=[], ordered_sensor=_ordered)
 
-
-if 0:
-    _adei = adeireader_obj['bess250']
-    _g = _adei.query_group('allgemein_500')
-    print map(_g.get, ['e_from_batt_act'])
-    print _adei.query_data('allgemein_500', ['e_from_batt_act'] )
+    for s in conf:
+        _adei = init_adei(s)
+        _group = init_group(s.get('sensors'))
+        adei[c]['db'].append({'adei': _adei, 'sensor_group': _group})
 
 
 class BessApi(Resource):
@@ -56,28 +60,26 @@ class BessApi(Resource):
 
         # test if configuration defined
         try:
-            adei = adeireader_obj[conf]
-            sns = sensors[conf]
-            ordered = sensors_ordered[conf]
+            db = adei[conf]['db']
+            ordered = adei[conf]['ordered_sensor']
         except KeyError:
             confs = ""
             for i in configurations: confs += '/%s' % i
             return output_html("Wrong query: possible configurations are %s" % confs, 404)
 
         data = {}
-        for grp, sns_list in sns.iteritems():
-            q = adei.query_data(grp, sns_list)
-            for sns, val in q.iteritems():
-                data['.'.join([grp, sns])] = val[0]
+        for _db in db:
+            _adei = _db.get('adei')
+            for grp, sns in _db.get('sensor_group').iteritems():
+                q = _adei.query_data(grp, sns)
+                for sns, val in q.iteritems():
+                    data['.'.join([grp, sns])] = val[0]
 
-        data_list = []
-        for s in ordered:
-            r = data.get(s['sensor']) or 'nan'
-            data_list.append(r)
+        data_list = [data.get(s) or 'nan' for s in ordered ]
         data_list = ['%.3f'%float(f)  for f in data_list]
 
-        # return output_html(data_list, 200)
-        return data
+        return output_html(data_list, 200)
+        # return data
 
 class BessInfo(Resource):
 
